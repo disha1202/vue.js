ffec1353ae2e62b8d2b282cafdac0c2ed960784e
disha1202 <58461801+disha1202@users.noreply.github.com>
2021-08-16 22:43:20 +0530

    implimentation of routing

---
+++ login_registration_components_router/src/views/login.vue
@@ -0,0 +1,66 @@
+<template>
+
+    <div class="login">
+        <form>
+           <div class="email">
+
+             <label for="email">E-mail </label>
+
+             <input  v-model="email" type="email">
+
+           </div>
+
+           <div class="password">
+           
+             <label for="password">Password </label>
+
+             <input  v-model="password" type="password">
+
+           </div>
+
+           <button @click="login" type="submit">Login</button>
+
+        </form>
+        <div>
+            Don't have an account?
+            <router-link to= "/register">Register</router-link>
+        </div>
+       
+
+    </div>
+
+</template>
+<script>
+// import main  from "@/main.js"
+export default {
+    name:"Login",
+    data(){
+        return {
+            email:'',
+            password:'',
+        }
+    },
+    methods:{
+        login:function(){
+             if(this.email==="dishatalreja1202@gmail.com" && this.password==="disha12"){
+            alert("Login successful");
+        }
+        else{
+            alert("No such user");
+        }
+        console.log("Login successful");
+        }
+    }
+}
+</script>
+<style scoped>
+.login{
+    margin:  auto;
+}
+.password, .email{
+    padding: 20px;
+}
+button{
+    margin-bottom: 10px;
+}
+</style>
---
+++ login_registration_components_router/src/views/registration.vue
@@ -0,0 +1,70 @@
+<template>
+     <div class="register">
+        <form>
+
+            <div class="name">
+
+             <label for="name">Name </label>
+
+             <input  v-model="name" type="text">
+
+           </div>
+
+           <div class="email">
+
+             <label for="email">E-mail </label>
+
+             <input  v-model="email" type="email">
+
+           </div>
+
+           <div class="password">
+           
+             <label for="password">Password </label>
+
+             <input  v-model="password" type="password">
+
+           </div>
+
+           <button @click="register" type="submit">Register</button>
+
+        </form>
+        <div>
+            Already have an account?
+            <router-link to= "/login">Login</router-link>
+        </div>
+     </div>         
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            name:'',
+            email:'',
+            password:'',
+        }
+    },
+    methods:{
+      register:function() {
+           localStorage.setItem('name',this.name);
+            localStorage.setItem('email',this.email);
+            localStorage.setItem('password',this.password);
+           console.log( window.localStorage);
+           alert("Registration Successful");
+      }
+    }
+}
+</script>
+
+<style scoped>
+.register{
+    margin: auto;
+}
+.name, .email, .password{
+    padding: 20px;
+}
+button{
+    margin-bottom: 10px;
+}
+</style>
