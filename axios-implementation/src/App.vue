b6f71afb22a8551f7522dc638747af756ee3f0d4
disha1202 <58461801+disha1202@users.noreply.github.com>
2021-08-27 13:39:29 +0530

    implementation of axios and fetch

---
+++ axios-implementation/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+ <random-products
+v-for="product in products"
+:key='product.id'
+:source="product.image"
+:title="product.title"
+:price="product.price"
+:details="product.description"
+ ></random-products>
+</template>
+
+<script>
+
+import RandomProducts from './components/RandomProducts.vue';
+import axios from 'axios';
+export default {
+  name: 'App',
+  data(){
+    return{
+   products:[]
+    }
+   
+  },
+  
+  components: {
+  
+    RandomProducts
+  },
+  
+  beforeMount(){
+// fetch('https://fakestoreapi.com/products?limit=5')
+//             .then(res=>res.json())
+//             .then(data=>{
+//               console.log(data);
+//               this.products = data;
+              
+//             });
+axios.get('https://fakestoreapi.com/products?limit=5').then(response => {
+  this.products = response.data;
+  console.log(response.data);
+});
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ axios-implementation/src/components/RandomProducts.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="product">
+         <img class="product-img" v-bind:src="source" />
+         <p>{{title}}</p>
+         <p>Price :{{ price}} </p>
+         <p>Product Details :{{details}}</p>
+        
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        source:{
+            type:String,
+            require:true
+        },
+        title:{
+            type:String,
+            require:true
+        },
+        price:{
+            type:Number,
+            require:true
+        },
+        details:{
+            type:String,
+            require:true
+        }
+    },
+    data(){
+        return{
+          
+        }
+    },
+   
+    mounted(){
+        // fetch('https://fakestoreapi.com/products?').then((response) => response.json()).then(data =>{
+        //     this.source = data.image;
+        //     this.title = data.title;
+        //     this.price = data.price;
+        //     this.details =  data.description;
+        // })
+        
+    },
+   
+ }
+</script>
+<style scoped>
+.product-img{
+    height: 200px;
+    width: 200px;
+}
+
+.product{
+    border: 5px solid black;
+    padding: 20px;
+    margin: 20px;
+    width: 30%;
+}
+</style>
